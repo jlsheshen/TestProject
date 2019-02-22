750a26314c54bd2f174e8d7fa7db13817364e0e1
jilin <505280855@qq.com>
2019-02-22 17:42:30 +0800

    添加tinker\2

---
+++ tinkermodule/src/main/java/com/edu/tinker/CustomPatchListener.java
@@ -0,0 +1,38 @@
+package com.edu.tinker;
+
+import android.content.Context;
+
+import com.imooc.util.Utils;
+import com.tencent.tinker.lib.listener.DefaultPatchListener;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+/**
+ * @author: vision
+ * @function: 1.较验patch文件是否合法  2.启动Service去安装patch文件
+ * @date: 17/5/11
+ */
+public class CustomPatchListener extends DefaultPatchListener {
+
+    private String currentMD5;
+
+    public void setCurrentMD5(String md5Value) {
+
+        this.currentMD5 = md5Value;
+    }
+
+    public CustomPatchListener(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int patchCheck(String path) {
+        //patch文件ms5较验
+        if (!Utils.isFileMD5Matched(path, currentMD5)) {
+
+            return ShareConstants.ERROR_PATCH_DISABLE;
+        }
+
+
+        return super.patchCheck(path);
+    }
+}
